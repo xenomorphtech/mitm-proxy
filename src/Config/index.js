f0726606d3e20025c1a6266501f5a34d113627c8
Arth K. Gajjar <iarthstar@gmail.com>
2020-04-04 14:43:35 +0530

    Updated: README.md

--- src/Config/index.js
+++ src/Config/index.js
@@ -21,12 +21,12 @@ const Environments = {
 
 const BASE_URL = {
   [LOCALHOST]   : "http://localhost:8080",
-  [DEVELOPMENT] : "https://dev-network-client.herokuapp.com",
-  [STAGING]     : "https://stage-network-client.herokuapp.com",
-  [PRODUCTION]  : "https://network-client.herokuapp.com"
+  [DEVELOPMENT] : "https://dev-netmarble.herokuapp.com",
+  [STAGING]     : "https://stage-netmarble.herokuapp.com",
+  [PRODUCTION]  : "https://netmarble.herokuapp.com"
 };
 
-export const HOST = BASE_URL[process.env.REACT_APP_CURRENT_ENV || Environments[DEVELOPMENT]];
+export const HOST = BASE_URL[process.env.REACT_APP_CURRENT_ENV || Environments[LOCALHOST]];
 
 export const API = "/network_api";
 export const ORIGIN = "WEB";
@@ -38,7 +38,5 @@ export const BACKEND_ENDPOINTS = {
 };
 
 export const DEFAULTS = {
-  localhost: {
-
-  }
+  localhost: {}
 };

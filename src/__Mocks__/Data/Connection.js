978e82f3af54e4f39737d01ca969e96d0e5f89ad
Arth K. Gajjar <iarthstar@gmail.com>
2020-04-21 20:14:31 +0530

    Updated: Connection mocks with Env Variables

--- src/__Mocks__/Data/Connection.js
+++ src/__Mocks__/Data/Connection.js
@@ -2,9 +2,11 @@ export const connection = (i) => ({
   connected: false,
   name: "Connection " + i,
   // host: "https://mitm-proxy-wss.herokuapp.com", // Faker.internet.ip(),
-  host: "http://localhost",
-  port: "8080",
+  host: process.env.WSS_HOST || "http://localhost",
+  port: process.env.WSS_PORT || "8080",
   live: Boolean(i % 2)
 });
+
+console.log("ENV", process.env.WSS_HOST, process.env.WSS_PORT);
 
 export const connections = [1, 2, 3, 4].map(connection);

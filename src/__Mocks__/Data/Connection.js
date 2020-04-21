bde5aa561c53d28010c43d0cbc5a3d9c7c667b86
Arth K. Gajjar <iarthstar@gmail.com>
2020-04-21 20:29:17 +0530

    Updated: Env Variables as per React Standards

--- src/__Mocks__/Data/Connection.js
+++ src/__Mocks__/Data/Connection.js
@@ -2,11 +2,9 @@ export const connection = (i) => ({
   connected: false,
   name: "Connection " + i,
   // host: "https://mitm-proxy-wss.herokuapp.com", // Faker.internet.ip(),
-  host: process.env.WSS_HOST || "http://localhost",
-  port: process.env.WSS_PORT || "8080",
+  host: process.env.REACT_APP_WSS_HOST || "http://localhost",
+  port: process.env.REACT_APP_WSS_PORT || "8080",
   live: Boolean(i % 2)
 });
-
-console.log("ENV", process.env.WSS_HOST, process.env.WSS_PORT);
 
 export const connections = [1, 2, 3, 4].map(connection);

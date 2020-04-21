d9b70e2724bce7446d77ef0b1129a9fcf060d38c
Arth K. Gajjar <iarthstar@gmail.com>
2020-04-21 20:57:48 +0530

    Fixed: Connection Values with Env Variables

--- src/__Mocks__/Data/Connection.js
+++ src/__Mocks__/Data/Connection.js
@@ -1,9 +1,11 @@
+const host = process.env.REACT_APP_WSS_HOST === undefined ? "http://localhost" : process.env.REACT_APP_WSS_HOST;
+const port = process.env.REACT_APP_WSS_PORT === undefined ? "8080" : process.env.REACT_APP_WSS_PORT;
+
 export const connection = (i) => ({
   connected: false,
   name: "Connection " + i,
-  // host: "https://mitm-proxy-wss.herokuapp.com", // Faker.internet.ip(),
-  host: process.env.REACT_APP_WSS_HOST || "http://localhost",
-  port: process.env.REACT_APP_WSS_PORT || "8080",
+  host,
+  port,
   live: Boolean(i % 2)
 });
 

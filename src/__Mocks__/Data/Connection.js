8d9a0527b4e4e7831881dfff5d24412fce37b9eb
Arth K. Gajjar <iarthstar@gmail.com>
2020-04-21 21:24:43 +0530

    Fixed: host and port values in connection

--- src/__Mocks__/Data/Connection.js
+++ src/__Mocks__/Data/Connection.js
@@ -1,5 +1,5 @@
-const host = process.env.REACT_APP_WSS_HOST === undefined ? "http://localhost" : process.env.REACT_APP_WSS_HOST;
-const port = process.env.REACT_APP_WSS_PORT === undefined ? "8080" : process.env.REACT_APP_WSS_PORT;
+const host = process.env.REACT_APP_WSS_HOST || "";
+const port = process.env.REACT_APP_WSS_PORT || "";
 
 export const connection = (i) => ({
   connected: false,
